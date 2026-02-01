5f4f035a70d8324cb188c9ed6cc9dd558ee4bfad
Rajatroshan <rajatroshan2002@gmail.com>
2026-02-02 02:40:35 +0530

    Project setup for microservices architecture - Added Config Service for centralized configuration management - Added Eureka Service for service discovery

---
+++ services/customer/src/main/java/com/rajat/ecommerce/customer/controller/CustomerController.java
@@ -0,0 +1,12 @@
+package com.rajat.ecommerce.customer.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+@RequiredArgsConstructor
+public class CustomerController {
+
+}
